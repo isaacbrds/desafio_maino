681d6843786cba7a5dd9defd94fdba4014021a2a
Isaac Brigido <isaac.brigido@gmail.com>
2024-04-18 10:07:45 -0300

    Feat: Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,8 @@ Rails.application.configure do
   config.hosts = [
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  "rails-production-8c6a.up.railway.app"
+  "rails-production-8c6a.up.railway.app",
+  /.*\.railway\.app/
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
